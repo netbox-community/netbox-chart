7fa9c203fd42b145e0d65bd72f486a4fa963cede
Léo Colombaro <git@colombaro.fr>
2024-09-05 17:08:24 +0000

    Lint configuration for python files

--- charts/netbox/files/ldap_config.py
+++ charts/netbox/files/ldap_config.py
@@ -1,54 +1,57 @@
-###################################################################
-#  This file serves as a LDAP configuration for Netbox            #
-#  https://netboxlabs.com/docs/netbox/en/stable/configuration/    #
-###################################################################
+"""
+This file serves as a LDAP configuration for Netbox
+https://netboxlabs.com/docs/netbox/en/stable/configuration/
+"""
 
 from functools import reduce
 from importlib import import_module
-from django_auth_ldap.config import LDAPSearch, LDAPGroupQuery
+from typing import Any
 
-import yaml
 import ldap
+import yaml
+from django_auth_ldap.config import LDAPGroupQuery, LDAPSearch
 
-def _load_yaml():
+
+def _load_yaml() -> None:
     """Load YAML from file"""
-    with open("/run/config/netbox/ldap.yaml", 'r', encoding='utf-8') as f:
+    with open("/run/config/netbox/ldap.yaml", "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)
     globals().update(config)
+
 
 def _read_secret(secret_name: str, secret_key: str, default: str | None = None) -> str | None:
     """Read secret from file"""
     try:
         f = open(
-          "/run/secrets/{name}/{key}".format(name=secret_name, key=secret_key),
-          'r',
-          encoding='utf-8'
+            f"/run/secrets/{secret_name}/{secret_key}",
+            "r",
+            encoding="utf-8",
         )
     except EnvironmentError:
         return default
-    else:
-        with f:
-            return f.readline().strip()
+    with f:
+        return f.readline().strip()
 
-# Import and return the group type based on string name
-def _import_group_type(group_type_name):
+
+def _import_group_type(group_type_name: str) -> Any | None:
+    """Import and return the group type based on name"""
     mod = import_module("django_auth_ldap.config")
     try:
         return getattr(mod, group_type_name)()
     except AttributeError:
         return None
 
+
 _load_yaml()
 
 # The following may be needed if you are binding to Active Directory.
-AUTH_LDAP_CONNECTION_OPTIONS = {
-    ldap.OPT_REFERRALS: 0
-}
+AUTH_LDAP_CONNECTION_OPTIONS = {ldap.OPT_REFERRALS: 0}
 
 # Set the DN and password for the NetBox service account if needed.
 AUTH_LDAP_BIND_PASSWORD = _read_secret("netbox", "ldap_bind_password")
 
-# This search ought to return all groups to which the user belongs. django_auth_ldap uses this to determine group
+# This search ought to return all groups to which the user belongs.
+# django_auth_ldap uses this to determine group
 # heirarchy.
 AUTH_LDAP_USER_SEARCH = LDAPSearch(
     AUTH_LDAP_USER_SEARCH_BASEDN,
@@ -63,7 +66,9 @@ AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
 AUTH_LDAP_GROUP_TYPE = _import_group_type(AUTH_LDAP_GROUP_TYPE)
 
 # Define a group required to login.
-AUTH_LDAP_REQUIRE_GROUP = reduce(lambda x, y: x | LDAPGroupQuery(y), AUTH_LDAP_REQUIRE_GROUP_LIST, False)
+AUTH_LDAP_REQUIRE_GROUP = reduce(
+    lambda x, y: x | LDAPGroupQuery(y), AUTH_LDAP_REQUIRE_GROUP_LIST, False
+)
 
 # Define special user types using groups. Exercise great caution when assigning superuser status.
 AUTH_LDAP_USER_FLAGS_BY_GROUP = {

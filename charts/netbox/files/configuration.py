afb90afec5bcb6bce969405e03e90d80102549f6
Léo Colombaro <git@colombaro.fr>
2024-09-05 17:08:13 +0000

    Split python files from configmap

---
+++ charts/netbox/files/configuration.py
@@ -0,0 +1,70 @@
+###################################################################
+#  This file serves as a base configuration for Netbox            #
+#  https://netboxlabs.com/docs/netbox/en/stable/configuration/    #
+###################################################################
+
+import re
+from pathlib import Path
+
+import yaml
+import os
+
+def _deep_merge(source, destination):
+    """Inspired by https://stackoverflow.com/a/20666342"""
+    for key, value in source.items():
+        dst_value = destination.get(key)
+
+        if isinstance(value, dict) and isinstance(dst_value, dict):
+            _deep_merge(value, dst_value)
+        else:
+            destination[key] = value
+
+    return destination
+
+def _load_yaml():
+    """Load YAML from files"""
+    extraConfigBase = Path("/run/config/extra")
+    configFiles = [Path("/run/config/netbox/netbox.yaml")]
+
+    configFiles.extend(sorted(extraConfigBase.glob("*/*.yaml")))
+
+    for configFile in configFiles:
+        with open(configFile, 'r', encoding='utf-8') as f:
+            config = yaml.safe_load(f)
+        _deep_merge(config, globals())
+
+def _read_secret(secret_name: str, secret_key: str, default: str | None = None) -> str | None:
+    """Read secret from file"""
+    try:
+        f = open(
+          "/run/secrets/{name}/{key}".format(name=secret_name, key=secret_key),
+          'r',
+          encoding='utf-8'
+        )
+    except EnvironmentError:
+        return default
+    else:
+        with f:
+            return f.readline().strip()
+
+CORS_ORIGIN_REGEX_WHITELIST = list()
+DATABASE = dict()
+EMAIL = dict()
+REDIS = dict()
+
+_load_yaml()
+
+DATABASE["PASSWORD"] = _read_secret("netbox", "db_password")
+EMAIL["PASSWORD"] = _read_secret("netbox", "email_password")
+REDIS["tasks"]["PASSWORD"] = _read_secret("netbox", "redis_tasks_password")
+REDIS["caching"]["PASSWORD"] = _read_secret("netbox", "redis_cache_password")
+SECRET_KEY = _read_secret("netbox", "secret_key")
+
+# Post-process certain values
+CORS_ORIGIN_REGEX_WHITELIST = [re.compile(r) for r in CORS_ORIGIN_REGEX_WHITELIST]
+if REDIS['tasks']['SENTINELS']:
+  REDIS['tasks']['SENTINELS'] = [tuple(x.split(r":")) for x in REDIS['tasks']['SENTINELS']]
+if REDIS['caching']['SENTINELS']:
+  REDIS['caching']['SENTINELS'] = [tuple(x.split(r":")) for x in REDIS['caching']['SENTINELS']]
+if ALLOWED_HOSTS_INCLUDES_POD_ID:
+  ALLOWED_HOSTS.append(os.getenv("POD_IP"))
